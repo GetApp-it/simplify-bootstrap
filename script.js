175bf7d91733bf33f20b9531fcd737f08685c37f
Stefano Valente <92720697+stefanovalentegva@users.noreply.github.com>
2024-04-03 14:32:26 +0200

    chore: -Removed jQuery - RGB colors

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const DEFAULT_FONT_FAMILY = '"Georama", sans-serif';
 const DEFAULT_PRIMARY_COLOR = '#9d33e7';
 const DEFAULT_SECONDARY_COLOR = '#103875';
 const CSS_FILENAME = 'style.css';
@@ -21,16 +22,25 @@ async function getCssTemplate() {
   return response.text();
 }
 
-async function handleDownloadButtonClick(event) {
+function convertColorToRgb(color) {
+  const r = parseInt(color.substr(-6, 2), 16);
+  const g = parseInt(color.substr(-4, 2), 16);
+  const b = parseInt(color.substr(-2), 16);
+  return [r, g, b].join(',');
+}
+
+function getCustomSettings() {
   let primaryColor = document.getElementById('primary-color').value;
   if (primaryColor === '') {
     primaryColor = DEFAULT_PRIMARY_COLOR;
   }
+  const primaryColorRgb = convertColorToRgb(primaryColor);
 
   let secondaryColor = document.getElementById('secondary-color').value;
   if (secondaryColor === '') {
     secondaryColor = DEFAULT_SECONDARY_COLOR;
   }
+  const secondaryColorRgb = convertColorToRgb(secondaryColor);
 
   let borderRadius = document.getElementById('border-radius').value;
   if (borderRadius === '') {
@@ -41,22 +51,92 @@ async function handleDownloadButtonClick(event) {
   if (fontSize === '') {
     fontSize = DEFAULT_FONT_SIZE;
   }
+  return {
+    primaryColor,
+    primaryColorRgb,
+    secondaryColor,
+    secondaryColorRgb,
+    borderRadius,
+    fontSize,
+  };
+}
+
+async function handleDownloadButtonClick(event) {
+  const customSettings = getCustomSettings();
 
   const cssTemplate = await getCssTemplate();
 
-  const customCss = cssTemplate
-    .replace(/{{PRIMARY_COLOR}}/g, primaryColor)
-    .replace(/{{SECONDARY_COLOR}}/g, secondaryColor)
-    .replace(/{{BORDER_RADIUS}}/g, borderRadius)
-    .replace(/{{FONT_SIZE}}/g, fontSize);
+  const customCss = replaceCustomSettings(cssTemplate, customSettings);
 
   download(CSS_FILENAME, customCss);
+
   event.stopPropagation();
   return false;
 }
+
+function replaceCustomSettings(
+  template,
+  {
+    primaryColor,
+    primaryColorRgb,
+    secondaryColor,
+    secondaryColorRgb,
+    borderRadius,
+    fontSize,
+  }
+) {
+  return template
+    .replace(/{{PRIMARY_COLOR}}/g, primaryColor)
+    .replace(/{{PRIMARY_COLOR_RGB}}/g, primaryColorRgb)
+    .replace(/{{SECONDARY_COLOR}}/g, secondaryColor)
+    .replace(/{{SECONDARY_COLOR_RGB}}/g, secondaryColorRgb)
+    .replace(/{{BORDER_RADIUS}}/g, borderRadius)
+    .replace(/{{FONT_FAMILY}}/g, DEFAULT_FONT_FAMILY)
+    .replace(/{{FONT_SIZE}}/g, fontSize);
+}
+
+function handleCustomStyleChange() {
+  const customSettings = getCustomSettings();
+  const cssTemplate = `<style> 
+    :root {
+      --bs-primary: {{PRIMARY_COLOR}};
+      --bs-primary-rgb: {{PRIMARY_COLOR_RGB}};
+      --bs-secondary: {{SECONDARY_COLOR}};
+      --bs-secondary-rgb: {{SECONDARY_COLOR_RGB}};
+      --bs-border-radius: {{BORDER_RADIUS}};
+    }
+    </style>`;
+  const customCss = replaceCustomSettings(cssTemplate, customSettings);
+  document.getElementById('custom-style').innerHTML = customCss;
+}
+
+function includePageElement(elementId, template) {
+  fetch(template)
+    .then((response) => response.text())
+    .then((body) => {
+      document.getElementById(elementId).innerHTML = body;
+    });
+}
+
 window.onload = () => {
+  // Load page elements
+  includePageElement('inc-nav', '/inc/nav.html');
+  includePageElement('inc-tab', '/inc/tab.html');
+  includePageElement('inc-button', '/inc/button.html');
+  includePageElement('inc-form', '/inc/form.html');
+  includePageElement('inc-alert', '/inc/alert.html');
+  includePageElement('inc-table', '/inc/table.html');
+  includePageElement('inc-accordion', '/inc/accordion.html');
+  includePageElement('inc-offcanvas', '/inc/offcanvas.html');
+  includePageElement('inc-modal', '/inc/modal.html');
+  includePageElement('inc-image', '/inc/image.html');
+
   // bindings
   document
     .getElementById('download-btn')
     .addEventListener('click', handleDownloadButtonClick);
+
+  document
+    .getElementById('custom')
+    .addEventListener('change', handleCustomStyleChange);
 };
